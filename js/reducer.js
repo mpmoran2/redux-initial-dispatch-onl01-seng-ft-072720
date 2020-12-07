cf031002f07d27adc5aa58e2a28e6d93d31a9fe4
mpmoran <kamokumoran@gmail.com>
2020-12-07 11:01:21 -0800

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,28 +1,23 @@
 let state;
  
-function reducer(state = {count: 0}, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
-  }
-}
+function changeState(state = { count: 0 }, action) {
+    switch (action.type) {
  
-function dispatch(action){
-  state = reducer(state, action);
-  render();
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 }
+ 
+      default:
+        return state;
+    }
+  }
+ 
+  function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
  
 function render(){
-  let container = document.getElementById('container');
-  container.textContent = state.count;
+  document.body.textContent = state.count
 }
  
 dispatch({type: '@@INIT'})
-
-let button = document.getElementById('button');
- 
-button.addEventListener('click', () => {
-  dispatch({type: 'INCREASE_COUNT'})
-})
